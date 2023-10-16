cd17126bada8608ab2a4523eb424bcdcde8909b1
Omar Faruk <banyars@Mins-MacBook-Pro.local>
2023-10-16 20:50:49 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,83 @@
+let lesson = [
+    {
+    "id":"1",  
+    "pic": "https://thenounproject.com/api/private/icons/1051526/edit/?backgroundShape=SQUARE&backgroundShapeColor=%23000000&backgroundShapeOpacity=0&exportSize=752&flipX=false&flipY=false&foregroundColor=%23000000&foregroundOpacity=1&imageFormat=png&rotation=0",
+    "name": "Math",
+    "city": "London",
+    "price": "300",
+    "avail":"10"
+    },
+    {
+    "id":"2",  
+    "pic": "https://cdn4.iconfinder.com/data/icons/world-flags-12/512/Untitled-2-05-512.png",
+    "name": "Spanish",
+    "city": "Manchester",
+    "price": "200",
+    "avail":"20"
+    },
+    {
+    "id":"3",  
+    "pic": "https://cdn1.iconfinder.com/data/icons/hotel-city-and-travel-ui-next-2020/16/CITY_speak-english-512.png",
+    "name": "English",
+    "city": "Birmingham",
+    "price": "110",
+    "avail":"13"
+    },
+    {
+    "id":"4",  
+    "pic": "https://cdn3.iconfinder.com/data/icons/education-science-vol-2-1/512/atom_science_physics_energay-128.png",
+    "name": "Physics",
+    "city": "Leeds",
+    "price": "100",
+    "avail":"16"
+    },
+    {
+    "id":"5",  
+    "pic": "https://cdn2.iconfinder.com/data/icons/study-education-3/512/Lab_tube-512.png",
+    "name": "Chemistry",
+    "city": "London",
+    "price": "400",
+    "avail":"4"
+    },
+    {
+    "id":"6", 
+    "pic": "https://cdn4.iconfinder.com/data/icons/biochemistry-3/64/Genetic-dna-magnify-chromosome-biology-science-chemistry-512.png",
+    "name": "Biology",
+    "city": "Sheffield",
+    "price": "100",
+    "avail":"10"
+    },
+    {
+    "id":"7",  
+    "pic": "https://cdn2.iconfinder.com/data/icons/designer-skills/128/code-programming-javascript-software-develop-command-language-512.png",
+    "name": "Introduction to JavaScript",
+    "city": "London",
+    "price": "45",
+    "avail":"6"
+    },
+    {
+    "id":"8",  
+    "pic": "https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/181_Java_logo_logos-512.png",
+    "name": "Introduction to Java",
+    "city": "Bristol",
+    "price": "60",
+    "avail":"3"
+    },
+    {
+    "id":"9", 
+    "pic": "https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/267_Python_logo-512.png",
+    "name": "Introduction to Python",
+    "city": "Liverpool",
+    "price": "75",
+    "avail":"8"
+    },
+    {
+    "id":"10", 
+    "pic": "https://cdn0.iconfinder.com/data/icons/programming-1-1/32/Programming_C-3-512.png",
+    "name": "Introduction to C++",
+    "city": "Birmingham",
+    "price": "90",
+    "avail":"7"
+    },
+
+]
